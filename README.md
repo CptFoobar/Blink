6f108de8eee01e3874fcfc6d533aca6d5e8cfe32
Sidhant Sharma [:TheTiger] <tigerkid001@gmail.com>
2015-07-24 12:05:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Savvy
+An add-on that makes the new tab much more productive.
+
+Thanks to CoDrops (http://tympanus.net/codrops/2015/02/26/inspiration-button-styles-effects/) for the awesome set of inspiring buttons.
---
+++ index.js
@@ -0,0 +1,28 @@
+const { Cc, Ci, Cu } = require("chrome");
+const windowMediator = Cc["@mozilla.org/appshell/window-mediator;1"]
+                     .getService(Ci.nsIWindowMediator);
+const self = require('sdk/self');
+const services = require("sdk/preferences/service");
+const newTabURL = self.data.url("sources/tab.html");
+
+
+const setNewTabURL = function() {
+	/* Set new tab source */
+	services.set("browser.newtab.url", self.data.url("sources/tab.html"));
+	/* TODO: Use better approach given here: 
+	https://mike.kaply.com/2012/06/21/best-practices-for-overriding-the-new-tab-page-with-your-extension/
+	*/
+};
+
+const clearTabUrlbar = function() {
+	/* Clear New tab's url bar. */
+	let windows = windowMediator.getEnumerator(null);
+	while (windows.hasMoreElements()) {
+		let window = windows.getNext();
+		window.gInitialPages.push(newTabURL);
+	}
+};
+
+setNewTabURL();
+clearTabUrlbar();
+
