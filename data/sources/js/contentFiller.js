a0f2e6c0394280441061f79143f4f6021f054570
Sidhant Sharma [:TheTiger] <tigerkid001@gmail.com>
2015-08-09 10:34:50 +0530

    Fixed more AMO warnings, added review link

--- data/sources/js/contentFiller.js
+++ data/sources/js/contentFiller.js
@@ -20,7 +20,7 @@ function addContent(url) {
     dataType: 'json',
     success: function(data) {
       $.each(data.responseData.feed.entries, function(key, value) {
-        var title = capitaliseFirstLetter(value.title);
+        var title = value.title;
         var imageSource = getImageSource(value.content);
         var contentSnippet = value.contentSnippet;
         var link = value.link;
@@ -36,30 +36,16 @@ function addContent(url) {
 }
 
 function getImageSource(source) {
-  var div = document.createElement("div");
-  div.innerHTML = source;
+  var parser = new DOMParser(), 
+      doc = parser.parseFromString(source, "text/html");
+  var div = doc.body;
   var imgsrc = div.getElementsByTagName('img')[0].src;
   if(imgsrc.length > 5 && imgsrc.indexOf("rc.feedsportal.com") == -1) {
-      console.log(imgsrc);
       return imgsrc;
     }
   else return "resource://@blink/data/icons/default_icon.jpg"
 }
 
-function getContentHeader(content) {
-  var div = document.createElement("div");
-  div.innerHTML = content;
-  var text = div.textContent || div.innerText || "";
-  return text;
-}
-
-/**
- * Capitalizes the first letter of any string variable
- * source: http://stackoverflow.com/a/1026087/477958
- */
-function capitaliseFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
 function newHope(title, imageSource, contentSnippet, link) {
     var card = document.createElement('div');
