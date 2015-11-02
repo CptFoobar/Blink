5d1bc51b93004f661515eda3311c0b1e52eb04f7
Sidhant Sharma [:TigerKid001] <tigerkid001@gmail.com>
2015-11-02 20:16:45 +0530

    Switched to FAB from sidenav

--- data/js/MainController.js
+++ data/js/MainController.js
@@ -1,62 +1,48 @@
 (function() {
     var app = angular.module('blink');
 
-    var MainController = function($scope, $timeout, $mdSidenav, $log) {
-            $scope.toggleLeft = buildDelayedToggler('left');
-            /**
-             * Supplies a function that will continue to operate until the
-             * time is up.
-             */
-            function debounce(func, wait, context) {
-                var timer;
-
-                return function debounced() {
-                    var context = $scope,
-                        args = Array.prototype.slice.call(arguments);
-                    $timeout.cancel(timer);
-                    timer = $timeout(function() {
-                        timer = undefined;
-                        func.apply(context, args);
-                    }, wait || 10);
-                };
-            }
-
-            /**
-             * Build handler to open/close a SideNav; when animation finishes
-             * report completion in console
-             */
-            function buildDelayedToggler(navID) {
-                return debounce(function() {
-                    $mdSidenav(navID)
-                        .toggle()
-                        .then(function() {
-                            $log.debug("toggle " + navID + " is done");
-                        });
-                }, 200);
-            }
-
-            function buildToggler(navID) {
-                return function() {
-                    $mdSidenav(navID)
-                        .toggle()
-                        .then(function() {
-                            $log.debug("toggle " + navID + " is done");
-                        });
-                }
-            }
-
-            $scope.close = function() {
-                $mdSidenav('left').close()
-                    .then(function() {
-                        $log.debug("close LEFT is done");
-                    });
-
-            };
-
+    var MainController = function($scope, $location) {
+        var self = this;
+        self.isOpen = false;
+        self.isTbOpen = false;
+        self.Menu = {
+            label : "Menu",
+            icon : "fa-plus"
         };
+        self.items = [{
+            name: "Content",
+            icon: "fa-rss-square",
+            direction: "right",
+            target : '/content'
+        }, {
+            name: "Settings",
+            icon: "fa-cog",
+            direction: "right",
+            target : '/content'
+        }, {
+            name: "Help",
+            icon: "fa-question",
+            direction: "right",
+            target : '/content'
+        }, {
+            name: "About",
+            icon: "fa-info",
+            direction: "right",
+            target : '/content'
+        }];
+
+        $scope.goTo = function(path) {
+            console.log("Going to: " + path + " " + typeof path);
+            $location.path(path);
+        }
+
+        $scope.toggleMenu = function() {
+            self.isOpen = !self.Open;
+        }
+    };
 
 
 
-        app.controller('MainController', ['$scope', '$timeout', '$mdSidenav', '$log', MainController]);
+    app.controller('MainController', ['$scope', '$location', MainController]);
 
 }());
