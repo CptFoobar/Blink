6cf49e479abc30e0c4f728d7e94b4d259c2a081e
Sidhant Sharma <sidhant.sharma1208@gmail.com>
2019-04-14 22:16:16 +0530

    Add all other components. All links work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
+import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelpComponent } from './components/help/help.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ContentComponent } from './components/content/content.component';
 import { FeedComponent } from './components/feed/feed.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HistoryComponent } from './components/history/history.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'feed', component: FeedComponent },
+  { path: 'recent', component: HistoryComponent },
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'blink/content', component: ContentComponent },
+  { path: 'blink/settings', component: SettingsComponent },
+  { path: 'blink/help', component: HelpComponent },
+  { path: 'blink/about', component: AboutComponent },
   { path: '',
     redirectTo: 'home',
     pathMatch: 'full'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { HomeComponent } from './components/home/home.component';
 import { FeedComponent } from './components/feed/feed.component';
 import { WindowScrollComponent } from './components/window-scroll/window-scroll.component';
 import { UniquePipe } from './pipes/unique.pipe';
+import { ContentComponent } from './components/content/content.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelpComponent } from './components/help/help.component';
+import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
+import { HistoryComponent } from './components/history/history.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,13 @@ import { UniquePipe } from './pipes/unique.pipe';
     HomeComponent,
     FeedComponent,
     WindowScrollComponent,
-    UniquePipe
+    UniquePipe,
+    ContentComponent,
+    SettingsComponent,
+    AboutComponent,
+    HelpComponent,
+    BookmarksComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
