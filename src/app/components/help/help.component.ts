ab8c76b2da9240f7168e0777db983a265c7aac23
Sidhant Sharma <sidhant.sharma1208@gmail.com>
2019-04-17 21:58:49 +0530

    Add help

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -12,4 +12,22 @@ export class HelpComponent implements OnInit {
   ngOnInit() {
   }
 
+  private elemOffset(el) {
+    let rect = el.getBoundingClientRect();
+    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: Math.max(0, rect.top + scrollTop - 100), left: rect.left + scrollLeft };
+  }
+
+  scrollToElement(id: string) {
+    let div = document.querySelector(id);
+    let divOffset = this.elemOffset(div);
+    console.log(divOffset);
+    try {
+      window.scrollTo({ top: divOffset.top, left: divOffset.left, behavior: 'smooth' });
+    } catch (e) {
+      window.scrollTo(divOffset.top, divOffset.left);
+    }
+  }
+
 }
